8a2f2ba6bf8f59cc8219e250f45416d109a4fe55
Bamshak90 <lilmarkhat@gmail.com>
2025-07-08 09:45:09 +0100

    Added some practice done on day three

--- python/strings.py
+++ python/strings.py
@@ -17,5 +17,47 @@ multi_user = '''
     and also I love talking
 '''
 
+'''
+
+company_name = "Blockfuse"
+
+print(company_name[0])
+print(company_name[1])
+print(company_name[2])
+print(company_name[3])
+print(company_name[4])
+print(company_name[5])
+print(company_name[6])
+print(company_name[7])
+print(company_name[8])
 
 print(multi_user)
+
+
+'''
+
+
+qoute = "Five slice of bread"
+
+print(qoute[0:4])
+print(qoute[5:10])
+print(qoute[11:13])
+print(qoute[14:])
+
+chain = "Blockchain"
+number = 50
+total = (len(chain)) + number + 1
+print(total)
+
+technology = "programmer"
+
+first_half = technology[-7:10]
+second_half = technology[-10:-7]
+
+
+print(first_half.upper())
+
+print(second_half.upper())
+
+print(first_half.lower())
+
