c10dd56b84fdf870557c034c1616dd00edeb14c7
Bamshak90 <lilmarkhat@gmail.com>
2025-07-30 09:00:46 +0100

    Dictionary practice to help for class tomorrow

---
+++ python/Day10/tuples/exercise_2.py
@@ -0,0 +1,11 @@
+fruits = ("apple", "banana", "coconut", "mango")
+
+print(fruits)
+
+response = { "success": True,
+            "message": "Welcome",
+             "email": "mark@gmail.com",
+            "username": "Mark"
+}
+
+print(response)
