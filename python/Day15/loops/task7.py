ec86bd98ce6a311a1377d0d6c80e3c54b08a44ef
Bamshak90 <lilmarkhat@gmail.com>
2025-08-12 16:06:05 +0100

    key changes

--- python/Day15/loops/task7.py
+++ python/Day15/loops/task7.py
@@ -1,12 +1,18 @@
+'''
+print the numbers in a reverse other with out using any method
+
+print the largest number
+'''
+
 numbers = [12,75,150,180,145,525,50]
 
-'''
+
 count = len(numbers) - 1
 for number in range(len(numbers)):
-    print(number[count])
+    print(numbers[count])
     count = count -1
 
-'''
+
 big = 0
 
 for number in numbers:
