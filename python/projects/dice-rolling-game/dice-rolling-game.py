1252bb96e5184608628582519a1f0a55d344a689
Bamshak90 <lilmarkhat@gmail.com>
2025-08-26 19:24:19 +0100

    dice rolling game

--- python/projects/dice-rolling-game/dice-rolling-game.py
+++ python/projects/dice-rolling-game/dice-rolling-game.py
@@ -5,11 +5,14 @@ while True:
     choice = input("Do you want to roll a dice?(\"Y/Na\"): ").lower()
 
     if choice == "y":
-        rand = random.randint(1,30)
-        print(rand)
+        rand1 = random.randint(1,10)
+        rand2 = random.randint(1,20)
+        print(f"You roll {rand1} and {rand2}")
     elif choice == "n":
         print("Thank you for playing")
         break
+    else:
+        print("Invalid choice")
 
 
 
