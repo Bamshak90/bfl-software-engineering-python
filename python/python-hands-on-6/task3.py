aae5eda84e5f6ab7f50e96d5d70b99402caa8e5c
Bamshak90 <lilmarkhat@gmail.com>
2025-08-29 16:08:36 +0100

    solved task 1 to 3

---
+++ python/python-hands-on-6/task3.py
@@ -0,0 +1,41 @@
+"""
+Write a function calculate_grade(score) that takes a student’s numeric score (an integer between 0 and 100) and returns their letter grade based on the following rules:
+90–100 → A
+80–89 → B
+70–79 → C
+60–69 → D
+Below 60 → F
+Make sure your function works for all boundary values (e.g., 60, 70, 80, 90, 100).
+
+Expected output
+print(calculate_grade(95))   # A
+print(calculate_grade(88))   # B
+print(calculate_grade(72))   # C
+print(calculate_grade(65))   # D
+print(calculate_grade(40))   # F
+print(calculate_grade(100))  # A
+print(calculate_grade(60))   # D
+print(calculate_grade(70))   # C
+print(calculate_grade(89))   # B
+"""
+
+def start():
+    score = float(input("Enter your score: "))
+    if score == score:
+        calculate_grade(score)
+
+
+
+def calculate_grade(score):
+    if score >= 90 and score <= 100:
+        print("A")
+    elif score >= 80 and score <= 89:
+        print("B")
+    elif score >= 70 and score <= 79:
+        print("C")
+    elif score >= 60 and score <= 69:
+        print("D")
+    else:
+        print("F")
+
+start()
