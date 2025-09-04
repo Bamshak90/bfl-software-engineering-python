529fad37274f318be3d43a3d56757b743db2f4b5
Bamshak90 <lilmarkhat@gmail.com>
2025-09-04 22:03:46 +0100

    solved banking

--- python/assignments/python-assignment-8/bank.py
+++ python/assignments/python-assignment-8/bank.py
@@ -13,3 +13,46 @@ Example Usage:
     acc.withdraw(200)
     print(acc.get_balance())  # 1300
 """
+
+class BankAccount:
+  def __init__(self):
+    self.customer = {}
+  
+  def register_customer(self, name, balance):
+    if name not in self.customer:
+      self.customer[name] = balance
+      print(f"{name} has register successfully")
+
+    else: 
+      print(f"{name} is already a customer")
+
+  def deposit_amount(self, name, balance):
+    if name in self.customer:
+      self.customer[name] += balance
+      print(f"{name} you have deposited {balance} to your account")
+      print(self.customer)
+    else:
+      print(f"{name}, You are not a customer")
+
+  def withdraw_amount(self, name, balance):
+    if name in self.customer:
+      if self.customer[name] >= balance:
+        self.customer[name] -= balance
+        print(f"{name}, You have successfully withdrawed {balance}")
+        print(self.customer)
+      else:
+        print(f"{name}, you don't have sufficient balance")
+    else:
+      print(f"{name}, You are not a customer")
+
+  def check_balance(self, name):
+    if name in self.customer:
+        print(f"{name}, your balance is: {self.customer[name]}")
+    else:
+        print(f"{name}, you are not a customer")
+
+account = BankAccount()
+account.register_customer("Tory", 1000)
+account.deposit_amount("Tory", 10000)
+account.withdraw_amount("Tory", 100000)
+account.check_balance("Tory")
