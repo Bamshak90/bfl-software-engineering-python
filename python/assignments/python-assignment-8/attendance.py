043400867bee0e414fb20f13bbb4c3c86de03810
Bamshak90 <lilmarkhat@gmail.com>
2025-09-04 23:44:31 +0100

    solve attendance

--- python/assignments/python-assignment-8/attendance.py
+++ python/assignments/python-assignment-8/attendance.py
@@ -14,3 +14,48 @@ Example Usage:
     print(register.get_status("John"))   # "Present"
     print(register.show_register())      # {"John": "Present", "Mary": "Absent"}
 """
+
+
+class AttendanceRegister:
+    def __init__(self):
+        self.attendance = {} 
+
+    def register_student(self, name):
+        if name in self.attendance:
+            print(f"{name} is already registered")
+        else:
+            self.attendance[name] = "Not Marked"
+            print(f"{name} registered successfully")
+    
+    def mark_present(self, name):
+        if name in self.attendance:
+            self.attendance[name] = "Present"
+            print(f"{name} marked as Present")
+        else:
+            print(f"{name} is not registered")
+    
+    def mark_absent(self, name):
+        if name in self.attendance:
+            self.attendance[name] = "Absent"
+            print(f"{name} marked as Absent")
+        else:
+            print(f"{name} is not registered")
+    
+    def get_status(self, name):
+        return self.attendance.get(name, f"{name} is not registered")
+    
+    def show_register(self):
+        return self.attendance
+
+register = AttendanceRegister()
+register.register_student("Mark")
+register.register_student("Mary")
+register.register_student("mp")
+
+register.mark_present("Mark")
+register.mark_absent("Mary")
+
+print(register.get_status("Mark"))  
+print(register.get_status("mp"))   
+print(register.get_status("Mary")) 
+print(register.show_register())      
