04180bdbc9e2db3ea8c7a00e3da1fb601e214e1e
Bamshak90 <lilmarkhat@gmail.com>
2025-09-04 23:55:11 +0100

    solved calculator logic

--- python/assignments/python-assignment-8/calculator.py
+++ python/assignments/python-assignment-8/calculator.py
@@ -12,3 +12,29 @@ Example Usage:
     print(calc.add(5, 3))        # 8
     print(calc.divide(10, 0))    # "Error: Division by zero"
 """
+
+class Calculator:
+    def __init__(self):
+         pass 
+    
+    def add(self, num1, num2):
+         return num1 + num2
+
+    def subtract(self, num1, num2):
+         return num1 - num2
+
+    def multiply(self, num1, num2):
+         return num1 * num2
+
+    def divide(self, num1, num2):
+         if num2 == 0:
+              return "Error: division by zero"
+
+         return num1 / num2
+
+
+calc = Calculator()
+print(calc.add(5.8, 3)) 
+print(calc.subtract(5, 3)) 
+print(calc.multiply(5, 3)) 
+print(calc.divide(5, 3)) 
